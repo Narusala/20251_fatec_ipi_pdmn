f286d48cfa73cc07be66e7a85e924598c1d4712a
Nathan Lazzaro <rplazzaro@hotmail.com>
2025-03-07 14:16:56 -0300

    test: testa a divisao

--- intro_git/teste_calculadora.py
+++ intro_git/teste_calculadora.py
@@ -4,3 +4,4 @@ b = 3
 print(f'{a} + {b} = {Calculadora.somar(a, b)}')
 print(f'{a} - {b} = {Calculadora.subtrair(a, b)}')
 print(f'{a} * {b} = {Calculadora.multiplicar(a, b)}')
+print(f'{a} / {b} = {Calculadora.dividir(a, b)}')
