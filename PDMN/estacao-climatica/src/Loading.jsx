44429ac280b2f33b54f47802ab79dcbb14565cdd
Nathan Lazzaro <rplazzaro@hotmail.com>
2025-03-21 22:02:26 -0300

    learn(ap06-react): encerra a apostila

---
+++ PDMN/estacao-climatica/src/Loading.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+export class Loading extends Component {
+  render() {
+    return (
+        <div className="d-flex flex-column justify-content-center align-items-center border rounded p-3 w-100">
+            <div className='spinner-border text-primary' role='status'
+                style={{width: '3rem', height: '3rem'}}>
+                <span className='visually-hidden'>Carregando...</span>
+            </div>
+            <p className="mt -3 text-primary">
+                {this.props.mensagem}
+            </p>
+        </div>
+    )
+  }
+}
+
+Loading.defaultProps = {
+    mensagem: 'Carregando...'
+}
+
+export default Loading
